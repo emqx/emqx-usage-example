dba0745472cef0295ed7ccd553b82dfd54aee5af
zmstone <zmstone@gmail.com>
2024-10-18 21:43:52 +0200

    Simplify configuration

--- client-attributes/subscribe.sh
+++ client-attributes/subscribe.sh
@@ -7,4 +7,4 @@ USERNAME='sub-user-1'
 # This client is allowed to login with secret hash algorithm
 PASSWORD="$(echo -n "${USERNAME}:magic-chicken" | sha1sum | awk '{print $1}')"
 
-mqttx sub -h localhost -p 8883 -l mqtts --key certs/client.key --cert certs/client.pem --ca certs/ca.pem -i "${CLIENTID}" -u "${USERNAME}" -P "${PASSWORD}" -t 't/#'
+mqttx sub -h localhost -p 1883 -l mqtt -i "${CLIENTID}" -u "${USERNAME}" -P "${PASSWORD}" -t 't/#'
