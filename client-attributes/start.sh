8e1b2046179704f34cd67cd610de2c599d129c49
zmstone <zmstone@gmail.com>
2024-10-18 21:38:47 +0200

    No need for data dir

--- client-attributes/start.sh
+++ client-attributes/start.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
 set -euo pipefail
-
-mkdir -p data
-
-## EMQX in docker is running with `emqx` user, which has an unknown user ID to the host, so we change mode from host
-chmod 777 data
-
 docker compose up --build -d
